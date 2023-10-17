969584ee1ec5feae3150a501484dc341910299ab
Andre Goltz <andregoltz@outlook.com.br>
2023-10-17 13:41:42 -0300

    Configuring and Adjust API project

--- Login-System.Api/Program.cs
+++ Login-System.Api/Program.cs
@@ -1,4 +1,10 @@
+using Login_System.Api.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.AddConfiguration();
+builder.AddDatabase();
+builder.AddJwtAuthentication();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
